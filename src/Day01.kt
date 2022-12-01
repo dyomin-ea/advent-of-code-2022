496e96d934f4b4242ee98df31828876198baedf2
Дёмин Егор Александрович <eg.djomin@cft.ru>
2022-12-01 16:30:09 +0200

    Day01 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+	fun part1(input: List<String>): Int =
+		input.fold(mutableListOf(0)) { acc, s ->
+			acc += if (s.isEmpty()) {
+				0
+			} else {
+				acc.pop() + s.toInt()
+			}
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+			acc
+		}
+			.max()
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+	fun part2(input: List<String>): Int =
+		input.fold(mutableListOf(0)) { acc, s ->
+			acc += if (s.isEmpty()) {
+				0
+			} else {
+				acc.pop() + s.toInt()
+			}
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+			acc
+		}.sortedDescending()
+			.take(3)
+			.sum()
+
+	val testInput = readInput("Day01_test")
+	check(part1(testInput) == 24000)
+	check(part2(testInput) == 45000)
+
+	val input = readInput("Day01")
+	println(part1(input))
+	println(part2(input))
 }
